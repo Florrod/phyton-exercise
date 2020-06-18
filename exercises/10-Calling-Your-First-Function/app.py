ce1dec074eac9d1b6440f1491ea9d9bf44e5ef7f
Flor Rodriguez <florrod23@gmail.com>
2020-06-18 19:46:39 +0000

    Subo hasta el ejercicio 10

--- exercises/10-Calling-Your-First-Function/app.py
+++ exercises/10-Calling-Your-First-Function/app.py
@@ -1,6 +1,6 @@
 def is_odd(my_number):
-  	return (my_number % 2 != 0)
-
-
+    return(my_number % 2 != 0)
 def my_main_code():
-    # your code here
+   return(is_odd(45345))
+my_main_code()
+print(my_main_code())
